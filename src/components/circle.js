6ed6a79397958e1291015564d418fa4593c1732b
Shantanu <77541968+shantanu1998s@users.noreply.github.com>
2023-02-07 19:03:05 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,11 @@
 // import using require
-
+const Shape=require('./shape');
 // declare class
-
-
+class Circle extends Shape{
+    constructor(color){
+        super(color);
+    }
+    calculateArea(){}
+}
 // export class using module.exports
+module.exports=Circle;
